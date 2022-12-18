408b270571c8c068f02469e5ac816307ee6db9d0
bengris32 <bengris32@protonmail.ch>
2022-12-18 21:20:10 +0000

    RMX3395: Initial SELinux rules
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: I0189f14885f5f8836fc14f085928f1171751163d
    Signed-off-by: HELLINFIX  <mridulsharma57570@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -150,6 +150,8 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # SELinux
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
