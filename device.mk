71c70860f8cd0a04a97d53a7d11c8db803f6237a
HELLINFIX <mridulsharma57570@gmail.com>
2023-09-23 20:39:06 +0000

    RMX3395: Build default Soundtrigger 2.3 impl
    Change-Id: Id14996b1feba080620fb6e5ed8411ba962fdbac2
    Signed-off-by: HELLINFIX <mridulsharma57570@gmail.com>

--- device.mk
+++ device.mk
@@ -129,6 +129,10 @@ PRODUCT_PACKAGES += \
     ueventd.rc \
     ueventd.wifi.rc
 
+# Soundtrigger
+PRODUCT_PACKAGES += \
+    android.hardware.soundtrigger@2.3-impl
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
