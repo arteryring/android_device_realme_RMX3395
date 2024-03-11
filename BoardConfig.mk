4c5c0972bae3acf83560a3e15f2d089261875e8f
Sidharth <157048502+arteryring@users.noreply.github.com>
2024-03-11 15:49:01 +0530

    RMX3395: Revert cortex to a55 in BoardConfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,6 @@ AB_OTA_PARTITIONS += \
     odm
 BOARD_USES_RECOVERY_AS_BOOT := true
 
-
 # Inherit the proprietary files
 include vendor/realme/RMX3395/BoardConfigVendor.mk
 
@@ -31,14 +30,14 @@ TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 := 
 TARGET_CPU_VARIANT := generic
-TARGET_CPU_VARIANT_RUNTIME := cortex-a78
+TARGET_CPU_VARIANT_RUNTIME := cortex-a55
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv7-a-neon
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
-TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a78
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a55
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := RM6877
